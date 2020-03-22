bc996aef3b9a16a7701c4816cb420c06f0c9d40e
mofe64 <ogunbiyioladapo33@gmail.com>
2020-03-22 20:07:38 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const name = "Eyimofe Ogunbiyi";
+const courses = ["HTML", "CSS", "JAVASCRIPT", "NODE.JS"];
+
+console.log(`My name is ${name}, \nthe the courses i offer are ${courses}. `);
+for (i = 1; i <= 200; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
